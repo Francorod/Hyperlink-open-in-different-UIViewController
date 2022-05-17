b0bf8ca0e8a8bb806ef79890e3adc5acb65d6f54
Franco Rod <francorodios@gmail.com>
2022-05-17 11:26:45 -0400

    Initial Commit

---
+++ Hyperlink open in different UIViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hyperlink open in different UIViewController
+//
+//  Created by Franco Rodrigues on 5/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
